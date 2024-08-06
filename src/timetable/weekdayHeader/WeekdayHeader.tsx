3db40dadf11275ac03b2d676186d0a02ef0683f3
acoria <tewes.verena@gmail.com>
2024-08-06 21:22:30 +0200

    Fix timeline header bug

--- src/timetable/weekdayHeader/WeekdayHeader.tsx
+++ src/timetable/weekdayHeader/WeekdayHeader.tsx
@@ -12,26 +12,12 @@ export const WeekdayHeader: React.FC<IWeekdayHeaderProps> = (props) => {
       lengthOfWeekdayBlockWithColor +
       timelineColumn;
 
-    // return "1/1";
-    console.log(`${props.weekday} - ${from}/${to}`)
-    return `{${from}/${to}}`;
+    return `${from}/${to}`;
   };
-
-  // const weekdayGridColumn = (index: number) => {
-  //   const test = `${index * props.blockColumnSpan + 1 + timelineColumn}/${
-  //     index * 2 + 3 + timelineColumn
-  //   }`;
-  //   console.log(`${props.weekday} - ${test}`);
-  //   return test;
-  // };
-
-  // const weekdayGridColumn = (index: number) =>
-  //   `${index * props.blockColumnSpan + 1 + timelineColumn}/${
-  //     index * 2 + 3 + timelineColumn
-  //   }`;
 
   return (
     <div
+      key={props.weekday}
       className={styles.weekdayHeader}
       style={{
         gridColumn: weekdayGridColumn(props.positionInGrid),
